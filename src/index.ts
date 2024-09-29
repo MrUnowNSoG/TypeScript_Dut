e2d4b581f447a6b99f78300a319c85689c288b89
MrUnow <61953278+MrUnowNSoG@users.noreply.github.com>
2024-09-29 22:57:01 +0300

    [Update] animation

--- src/index.ts
+++ src/index.ts
@@ -11,40 +11,40 @@ modalTriggers.forEach((trigger) => {
 window.addEventListener('scroll', () => {
     const header = document.getElementById('header');
     if (window.scrollY > 50) {
-        header?.classList.remove('alt'); // Видаляємо клас 'alt' при прокрутці вниз
+        header?.classList.remove('alt');
     } else {
-        header?.classList.add('alt'); // Додаємо клас 'alt', якщо на початку сторінки
+        header?.classList.add('alt');
     }
 });
 
-// Відкрити меню при натисканні на <a href="#menu">
+// Відкрити меню
 const menuLink = document.querySelector('a[href="#menu"]');
 const menu = document.getElementById('menu');
 
 if (menuLink && menu) {
     menuLink.addEventListener('click', (event) => {
         event.preventDefault();
-        // Додаємо клас для body
+ 
         document.body.classList.add('is-menu-visible');
-        // Застосовуємо стилі для меню
+
         menu.style.pointerEvents = 'auto';
         menu.style.opacity = '1';
         menu.style.visibility = 'visible';
     });
 
-    // Закрити меню при натисканні поза межами меню
+    // Закрити меню при натисканні 
     window.addEventListener('click', (event) => {
         if (!menu.contains(event.target as Node) && event.target !== menuLink) {
-            // Видаляємо клас з body
+
             document.body.classList.remove('is-menu-visible');
-            // Повертаємо меню до початкових властивостей
+
             menu.style.pointerEvents = 'none';
             menu.style.opacity = '0';
             menu.style.visibility = 'hidden';
         }
     });
 
-    // Додатково, закриваємо меню, якщо натиснуто кнопку всередині меню
+    //закриваємо меню
     menu.querySelector('.close')?.addEventListener('click', () => {
         document.body.classList.remove('is-menu-visible');
         menu.style.pointerEvents = 'none';
@@ -53,7 +53,7 @@ if (menuLink && menu) {
     });
 }
 
-// Fetch API дані
+// Fetch
 const fetchData = async () => {
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -76,13 +76,10 @@ const displayPosts = (posts: any[]) => {
     });
     document.body.appendChild(postSection);
 };
-
-// Викликати fetch для отримання даних
 fetchData();
 
-// Видалення класу "is-preload" через 1 секунду після завантаження сторінки
 window.addEventListener('load', () => {
     setTimeout(() => {
         document.body.classList.remove('is-preload');
-    }, 1000); // 1000 мс = 1 секунда
+    }, 1000);
 });
